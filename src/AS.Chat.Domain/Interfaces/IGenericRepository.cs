fbae49d7ea5c0544ee947aec5e4861935143e5c7
Arthur Saito <arthur-saito@outlook.com>
2024-09-25 20:36:14 -0300

    [chore] repository

---
+++ src/AS.Chat.Domain/Interfaces/IGenericRepository.cs
@@ -0,0 +1,10 @@
+﻿using AS.Chat.Domain.Interfaces;
+
+namespace AS.Chat.Domain.SeedWork;
+
+public interface IGenericRepository<TAggregate> : IRepository where TAggregate: AggregateRoot<TAggregate>
+{
+    public Task Insert(TAggregate anAggregate, CancellationToken aCancellationToken);
+    public Task<TAggregate> GetById(Guid id, CancellationToken aCancellationToken);
+    public Task<IEnumerable<TAggregate>> GetAll();
+}
---
+++ src/AS.Chat.Domain/SeedWork/AggregateRoot.cs
@@ -0,0 +1,8 @@
+﻿using AS.Chat.Domain.Interfaces;
+
+namespace AS.Chat.Domain.SeedWork;
+
+public class AggregateRoot<T> : Entity<T> where T : IEntity
+{
+    
+}
